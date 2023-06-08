59356cdc56cc3300c902ad243026a96ad4ec7afc
objectbased <67526309+objectbased@users.noreply.github.com>
2023-06-08 13:40:32 -0400

    c
    c

---
+++ scripts/scanner_and_replace.py
@@ -0,0 +1,18 @@
+import os
+
+def scan_and_replace(directory):
+    for root, dirs, files in os.walk(directory):
+        for file_name in files:
+            file_path = os.path.join(root, file_name)
+            if os.path.isfile(file_path):
+                with open(file_path, 'r+') as file:
+                    content = file.read()
+                    updated_content = content.replace("${{ variable }}", "found")
+                    file.seek(0)
+                    file.write(updated_content)
+                    file.truncate()
+                print(f"Processed: {file_path}")
+
+# Usage example
+directory_path = '/path/to/parent/directory'
+scan_and_replace(directory_path)
