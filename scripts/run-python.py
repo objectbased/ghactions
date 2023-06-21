b0c3bbed314f0d38d4b33d1d54b8ed1fb7061134
objectbased <67526309+objectbased@users.noreply.github.com>
2023-06-21 16:03:07 -0400

    c
    c

--- scripts/run-python.py
+++ scripts/run-python.py
@@ -2,4 +2,9 @@ import hvac
 import os
 
 #print("::set-output name=value::test123")
-os.system('echo "{value}={test123}" >> $GITHUB_OUTPUT')
+#os.system('echo "{value}={test123}" >> $GITHUB_OUTPUT')
+
+name = 'token'
+value = 'test123'
+with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
+    print(f'{name}={value}', file=fh)
